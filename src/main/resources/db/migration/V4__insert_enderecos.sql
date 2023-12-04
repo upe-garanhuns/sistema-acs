04ac48ee7d1dd92c0ac7184864570cc2c2cece9d
amadeusantos <amadeusantos31@gmail.com>
2023-12-04 19:48:10 -0300

    [chore] add dump with users, requests and certificates

---
+++ src/main/resources/db/migration/V4__insert_enderecos.sql
@@ -0,0 +1,30 @@
+INSERT INTO public.endereco(
+        id,
+        uf,
+        bairro,
+        cep,
+        cidade,
+        complemento,
+        numero,
+        rua
+    )
+VALUES (
+        '1001',
+        'SP',
+        'Sé',
+        '01001-000',
+        'São Paulo',
+        'lado ímpar',
+        11,
+        'Praça da Sé'
+    ),
+    (
+        '1002',
+        'RS',
+        'Bom Jesus',
+        '91420-270',
+        'Porto Alegre',
+        '',
+        51,
+        'Rua São Domingos'
+    );
